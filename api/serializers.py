2b491bdc49d38529b482ed25b55253c4ea2e6602
tnlong1997 <tnlong1997@gmail.com>
2017-12-19 20:19:32 +0700

    add validations

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from api.models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(
+                required=True,
+                validators=[UniqueValidator(queryset=User.objects.all())]
+               )
+    password = serializers.CharField(min_length=8)
+
     class Meta:
         model = User
         fields = '__all__'
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -8,13 +8,21 @@ class UserListViewTest(APITestCase):
         self.user_list_url = reverse('user-list')
         self.data = {
             'username': 'test',
-            'password': 'test',
+            'password': 'testtest',
         }
 
-    def test_user_list_post(self):
+    def test_create_user(self):
         response = self.client.post(self.user_list_url, self.data)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(response.status_code, 201)
+
+    def test_create_user_with_short_password(self):
+        data = {
+            'username': 'short_password',
+            'password': 'short'
+        }
+        response = self.client.post(self.user_list_url, data)
+        self.assertEqual(response.status_code, 400)
 
     # def test_user_list_get(self):
     #     response = self.client.get(self.user_list_url, format='json')
